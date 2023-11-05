14a86fb1d586cd366408046660414bc326c781b1
Sebastian <mrseb10@gmail.com>
2023-11-05 23:10:32 +0100

    RWD Grid

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -11,21 +11,24 @@ const Feed = () => {
     },
   });
 
-  console.log(data);
-
   let countries = [];
 
   if (data) {
     countries = data.map((country: { [key: string]: any }) => {
       return (
-        <div className="bg-white cursor-pointer">
-          <div>
-            <img src={country.flags.png} alt={country.name.common} />
+        <div
+          key={uuidv4()}
+          className="bg-white cursor-pointer shadow-[0_0_7px_2px_rgba(0,0,0,0.06)]"
+        >
+          <div className="sm:h-[160px]">
+            <img
+              className="sm:h-full w-full"
+              src={country.flags.png}
+              alt={country.name.common}
+            />
           </div>
           <div className="p-6">
-            <p className="h2 text-black" key={uuidv4()}>
-              {country.name.common}
-            </p>
+            <p className="h2 text-black">{country.name.common}</p>
             <div className="flex flex-col gap-2 mt-4">
               <p className="h4 text-black">
                 <b>Population:</b> {country.population}
@@ -48,7 +51,9 @@ const Feed = () => {
       {isLoading ? (
         <div>Loading...</div>
       ) : (
-        <section className="px-20 py-12">{countries}</section>
+        <section className="px-20 py-12 grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-[72px] gap-y-[72px]">
+          {countries}
+        </section>
       )}
     </>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+
   theme: {
+    screens: {
+      lg: "1440px",
+      md: "1098px",
+      sm: "764px",
+    },
     extend: {
       colors: {
         black: "#111517",
